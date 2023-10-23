e88368ba527390b4d5d9f1ba3ca57c17174e8cf8
Mohammed alhamdi <104478279+wkwoeo@users.noreply.github.com>
2023-10-23 19:34:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-TOKEN = '...' #Сюда вы пишите свой токен. Узнать его можно в @BotFather
+TOKEN = '6367053421:AAEFzwWip6Y4JnO3T56zdNgyq_zE-qmZwro' #Сюда вы пишите свой токен. Узнать его можно в @BotFather
